1f67c7d43e12828b0666b7d8ad8d0411f60b9436
KyungJunNoh <developerjun0615@gmail.com>
2021-01-14 22:20:43 +0900

    join 전까지

--- main.js
+++ main.js
@@ -36,7 +36,6 @@ var app = http.createServer(function (request, response) {
         response.end(html);
       });
     } else {
-
       /*fs.readdir('./data', function (error, filelist) {
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
@@ -86,8 +85,7 @@ var app = http.createServer(function (request, response) {
     }
   } else if (pathname === '/create') {
     db.query(`SELECT * FROM topic`, function(error,topics){
-      var title = 'Welcome';
-      var description = 'Hello, Node.js';
+      var title = 'Create';
       var list = template.list(topics);
       var html = template.html(title, list,
         `
@@ -127,25 +125,31 @@ var app = http.createServer(function (request, response) {
       )
     });
   } else if (pathname === '/update') {
-    fs.readdir('./data', function (error, filelist) {
-      var filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        var title = queryData.id;
-        var list = template.list(filelist);
-        var html = template.html(title, list,
+    db.query('SELECT * FROM topic', function(error,topics){
+       // fs.readdir('./data', function (error, filelist) {
+        if(error){
+          throw error;
+        }
+      db.query(`SELECT * FROM topic WHERE id = ?`,[queryData.id], function(error2,topic){
+          if(error2){
+            throw error2;
+          }
+        // fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+        var list = template.list(topics);
+        var html = template.html(topic[0].title, list,
           `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <input type="hidden" name="id" value="${topic[0].id}">
+              <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
               <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
+                <textarea name="description" placeholder="description">${topic[0].description}</textarea>
               </p>
               <p>
                 <input type="submit">
               </p>
             </form>
             `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
         );
         response.writeHead(200);
         response.end(html);
@@ -156,18 +160,12 @@ var app = http.createServer(function (request, response) {
     request.on('data', function (data) {
       body = body + data;
     });
-    request.on('end', function () {
+    request.on('end', function() {
       var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-          response.writeHead(302, { Location: `/?id=${title}` });
-          response.end();
-          // 리다이렉션 : 출력의 방향을 바꾸는 것
-        })
-      });
+      db.query('UPDATE topic SET title = ?, description = ?, author_id = 1 WHERE id =?', [post.title,post.description,post.id], function(error,result){
+        response.writeHead(302, { Location: `/?id=${post.id}` });
+        response.end();
+      })
     });
   } else if (pathname === '/delete_process') {
     var body = '';
@@ -177,6 +175,16 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var id = post.id;
+      var filteredId = path.parse(id).base;
+      db.query('DELETE FROM topic WHERE id = ?', [post.id], function(error,reuslt){
+        if(error){
+          if(error){
+            throw error;
+          }
+        }
+        response.writeHead(302, { Location: `/` });
+        response.end();
+      })
       fs.unlink(`data/${id}`, function (error) {
         response.writeHead(302, { Location: `/` });
         response.end();
